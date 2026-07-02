e020b23f87d161d751cb1bdee2749fea7aa9658a
Shawn Guo <sguo37@jhu.edu>
2026-07-02 02:18:03 +0100

    [Feat] Sitemap Support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 // @ts-check
+import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://shawnguo.dev',
+	integrations: [sitemap()],
 });
